f0dc800ae682fa6050768624ae66145f29fcb38b
MenuraSudarshana <menurasudarshana03@gmail.com>
2026-05-17 00:38:41 +0530

    Update IAdminService

--- src/main/java/lk/ac/sliit/bakery_site/service/IAdminService.java
+++ src/main/java/lk/ac/sliit/bakery_site/service/IAdminService.java
@@ -1,11 +1,25 @@
 package lk.ac.sliit.bakery_site.service;
 
+import lk.ac.sliit.bakery_site.dto.AdminProductCreateRequestDto;
 import lk.ac.sliit.bakery_site.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.List;
 
 public interface IAdminService {
+
+    Map<String, Object> getSummary();
+
+    List<Product> getAllProducts();
+
+    Product createProduct(AdminProductCreateRequestDto request);
+
+    int importWebsiteCatalogProducts();
+
+    Product updateProduct(Integer productId, AdminProductCreateRequestDto request);
+
+    Product updateProductActive(Integer productId, boolean active);
 
     List<Product> getPublicProducts();
 }
