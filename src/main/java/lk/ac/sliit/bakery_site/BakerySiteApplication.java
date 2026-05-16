53fd6b6370382476194c1fd88b15e778e963c018
MenuraSudarshana <menurasudarshana03@gmail.com>
2026-05-16 20:55:00 +0530

    Initial Commit

---
+++ src/main/java/lk/ac/sliit/bakery_site/BakerySiteApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.bakery_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerySiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/sliit/bakery_site/BakerySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.bakery_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerySiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
