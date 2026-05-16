52a007fa68f9212172cf817889ad0a46004fb7da
MenuraSudarshana <menurasudarshana03@gmail.com>
2026-05-17 00:35:43 +0530

    create Admincontroller, AdminProductCreateRequestDto,AdminService

---
+++ src/main/java/lk/ac/sliit/bakery_site/dto/AdminProductCreateRequestDto.java
@@ -0,0 +1,78 @@
+package lk.ac.sliit.bakery_site.dto;
+
+public class AdminProductCreateRequestDto {
+
+    private String name;
+    private String category;
+    private String flavour;
+    private String description;
+    private String imageUrl;
+    private Double price;
+    private Double rating;
+    private Integer stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getFlavour() {
+        return flavour;
+    }
+
+    public void setFlavour(String flavour) {
+        this.flavour = flavour;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
+
